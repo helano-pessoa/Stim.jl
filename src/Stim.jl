3f41a324c98d85a42ee23dd39d903926fa91515b
helano-pessoa <hp@alu.ufc.br>
2021-02-08 20:19:55 -0300

    Add dantzig relaxation

--- src/Stim.jl
+++ src/Stim.jl
@@ -38,4 +38,26 @@ function knapsack(weights, profits, cap)
     return model, objective_value(model), value.(x)
 end
 
+export dantzig_relaxation
+
+function dantzig_relaxation(weights, profits, capacty)
+    density = [p/w for (p,w) in zip(profits, weights)]
+    sorted_density = reverse(sortperm(density))
+
+    aux_capacity = 0
+    x = zeros(length(profits))
+    for d in sorted_density
+        if (weights[d] + aux_capacity) <= capacity
+            x[d] = 1
+            aux_capacity += weights[d]
+        else
+            x[d] = (capacity - aux_capacity) / weights[d]
+        end
+    end
+
+    objective_value = dot(x, profits)
+
+    return objective_value, x
+end
+
 end # module
